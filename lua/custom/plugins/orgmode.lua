ed8c7518e4658296975ee0f67362adc3f265395f
John te Bokkel <tanj.tanj@gmail.com>
2024-04-02 08:19:30 -0700

    change orgmode meta_return keybind

--- lua/custom/plugins/orgmode.lua
+++ lua/custom/plugins/orgmode.lua
@@ -57,7 +57,7 @@ local M = {
     vim.api.nvim_create_autocmd('FileType', {
       pattern = 'org',
       callback = function()
-        vim.keymap.set('i', '<S-CR>', '<cmd>lua require("orgmode").action("org_mappings.meta_return")<CR>', {
+        vim.keymap.set('i', '<M-CR>', '<cmd>lua require("orgmode").action("org_mappings.meta_return")<CR>', {
           silent = true,
           buffer = true,
         })

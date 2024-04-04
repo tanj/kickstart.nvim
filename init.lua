92a7c251c2cc4633ab9ffe218ee291d2d13684f2
John te Bokkel <tanj.tanj@gmail.com>
2024-04-03 20:46:55 -0700

    yes we have a nerd font

--- init.lua
+++ init.lua
@@ -98,7 +98,7 @@ vim.opt.shell = 'cmd.exe'
 -- vim.opt.shellcmdflag = '-c'
 
 -- Set to true if you have a Nerd Font installed
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`

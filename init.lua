775cc8db6b4fe943e72a331b898d96f6fb874467
John te Bokkel <tanj.tanj@gmail.com>
2024-04-03 20:47:19 -0700

    use my modified zenburn as default theme

--- init.lua
+++ init.lua
@@ -753,7 +753,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'zenburn'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
@@ -761,7 +761,7 @@ require('lazy').setup({
   },
 
   {
-    'phha/zenburn.nvim',
+    'tanj/zenburn.nvim',
     -- semantic token highlights look like ass in here. Maybe fork and improve
     -- config = function() require("zenburn").setup() end
   },

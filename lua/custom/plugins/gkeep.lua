4946a71b0d4896b5798f74882d20a543dd5361ee
John te Bokkel <tanj.tanj@gmail.com>
2024-04-13 15:24:47 -0700

    note about gkeep not working

---
+++ lua/custom/plugins/gkeep.lua
@@ -0,0 +1,11 @@
+-- Right now this plugin doesn't work because gpsoauth issue
+-- https://github.com/simon-weber/gpsoauth/issues/59
+local M = {
+  'stevearc/gkeep.nvim',
+  build = 'UpdateRemotePlugins',
+  opts = {},
+  -- Optional dependencies
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+}
+-- return M
+return {}

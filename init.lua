da07aad26e6b335a67c47437b9a4db6daab55fb1
John te Bokkel <tanj.tanj@gmail.com>
2024-11-26 20:11:22 -0800

    fix: ruff_lsp deprecated in favour of ruff

--- init.lua
+++ init.lua
@@ -604,7 +604,7 @@ require('lazy').setup({
           end,
         },
         jedi_language_server = {},
-        ruff_lsp = {},
+        ruff = {},
         --
         rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs

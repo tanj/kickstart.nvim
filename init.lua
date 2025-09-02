1c423ce706262973ab5787961ed774e128531014
John te Bokkel <tanj.tanj@gmail.com>
2025-09-02 11:22:17 -0700

    feat: add vim-abolish

--- init.lua
+++ init.lua
@@ -263,6 +263,7 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-abolish',
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following

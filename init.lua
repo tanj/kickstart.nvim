4fc9456a1bfac2ff1b377e63cc1865da2b8a90db
John te Bokkel <tanj.tanj@gmail.com>
2025-12-24 07:29:37 -0800

    update to use main tree-sitter config style

--- init.lua
+++ init.lua
@@ -1090,11 +1090,57 @@ require('lazy').setup({
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    lazy = false,
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    opts = {
-      ensure_installed = {
+    branch = 'main',
+    -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
+    config = function()
+      ---@param buf integer
+      ---@param language string
+      local function treesitter_try_attach(buf, language)
+        -- check if parser exists and load it
+        if not vim.treesitter.language.add(language) then
+          return
+        end
+        -- enables syntax highlighting and other treesitter features
+        vim.treesitter.start(buf, language)
+
+        -- enables treesitter based folds
+        -- for more info on folds see `:help folds`
+        -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+
+        -- enables treesitter based indentation
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end
+
+      local available_parsers = require('nvim-treesitter').get_available()
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function(args)
+          local buf, filetype = args.buf, args.match
+          local language = vim.treesitter.language.get_lang(filetype)
+          if not language then
+            return
+          end
+
+          local installed_parsers = require('nvim-treesitter').get_installed 'parsers'
+
+          if vim.tbl_contains(installed_parsers, language) then
+            -- enable the parser if it is installed
+            treesitter_try_attach(buf, language)
+          elseif vim.tbl_contains(available_parsers, language) then
+            -- if a parser is available in `nvim-treesitter` enable it after ensuring it is installed
+            require('nvim-treesitter').install(language):await(function()
+              treesitter_try_attach(buf, language)
+            end)
+          else
+            -- try to enable treesitter features in case the parser exists but is not available from `nvim-treesitter`
+            treesitter_try_attach(buf, language)
+          end
+        end,
+      })
+
+      -- ensure basic parser are installed
+      local parsers = {
         'bash',
         'c',
         'c_sharp',
@@ -1113,111 +1159,102 @@ require('lazy').setup({
         'ziggy_schema',
         -- 'supermd',
         -- 'supermd_inline',
-      },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
-    config = function(_, opts)
-      -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-
-      ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup(opts)
-
-      -- There are additional nvim-treesitter modules that you can use to interact
-      -- with nvim-treesitter. You should go explore a few and see what interests you:
-      --
-      --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-      --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-      --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-      local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
-
-      parser_config.ziggy = {
-        install_info = {
-          url = 'https://github.com/kristoff-it/ziggy',
-          includes = { 'tree-sitter-ziggy/src' },
-          files = { 'tree-sitter-ziggy/src/parser.c' },
-          branch = 'main',
-          generate_requires_npm = false,
-          requires_generate_from_grammar = false,
-        },
-        filetype = 'ziggy',
       }
-
-      parser_config.ziggy_schema = {
-        install_info = {
-          url = 'https://github.com/kristoff-it/ziggy',
-          files = { 'tree-sitter-ziggy-schema/src/parser.c' },
-          branch = 'main',
-          generate_requires_npm = false,
-          requires_generate_from_grammar = false,
-        },
-        filetype = 'ziggy-schema',
-      }
-
-      parser_config.supermd = {
-        install_info = {
-          url = 'https://github.com/kristoff-it/supermd',
-          includes = { 'tree-sitter/supermd/src' },
-          files = {
-            'tree-sitter/supermd/src/parser.c',
-            'tree-sitter/supermd/src/scanner.c',
-          },
-          branch = 'main',
-          generate_requires_npm = false,
-          requires_generate_from_grammar = false,
-        },
-        filetype = 'supermd',
-      }
-
-      parser_config.supermd_inline = {
-        install_info = {
-          url = 'https://github.com/kristoff-it/supermd',
-          includes = { 'tree-sitter/supermd-inline/src' },
-          files = {
-            'tree-sitter/supermd-inline/src/parser.c',
-            'tree-sitter/supermd-inline/src/scanner.c',
-          },
-          branch = 'main',
-          generate_requires_npm = false,
-          requires_generate_from_grammar = false,
-        },
-        filetype = 'supermd_inline',
-      }
-
-      parser_config.superhtml = {
-        install_info = {
-          url = 'https://github.com/kristoff-it/superhtml',
-          includes = { 'tree-sitter-superhtml/src' },
-          files = {
-            'tree-sitter-superhtml/src/parser.c',
-            'tree-sitter-superhtml/src/scanner.c',
-          },
-          branch = 'main',
-          generate_requires_npm = false,
-          requires_generate_from_grammar = false,
-        },
-        filetype = 'superhtml',
-      }
-
-      vim.filetype.add {
-        extension = {
-          smd = 'supermd',
-          shtml = 'superhtml',
-          ziggy = 'ziggy',
-          ['ziggy-schema'] = 'ziggy_schema',
-          struct = 'st',
-          enum = 'st',
-        },
-      }
+      require('nvim-treesitter').install(parsers)
     end,
+    --   config = function(_, opts)
+    --     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+    --
+    --     ---@diagnostic disable-next-line: missing-fields
+    --     require('nvim-treesitter.config').setup(opts)
+    --
+    --     -- There are additional nvim-treesitter modules that you can use to interact
+    --     -- with nvim-treesitter. You should go explore a few and see what interests you:
+    --     --
+    --     --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+    --     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+    --     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+    --     local parser_config = require 'nvim-treesitter.parsers'
+    --
+    --     parser_config.ziggy = {
+    --       install_info = {
+    --         url = 'https://github.com/kristoff-it/ziggy',
+    --         includes = { 'tree-sitter-ziggy/src' },
+    --         files = { 'tree-sitter-ziggy/src/parser.c' },
+    --         branch = 'main',
+    --         generate_requires_npm = false,
+    --         requires_generate_from_grammar = false,
+    --       },
+    --       filetype = 'ziggy',
+    --     }
+    --
+    --     parser_config.ziggy_schema = {
+    --       install_info = {
+    --         url = 'https://github.com/kristoff-it/ziggy',
+    --         files = { 'tree-sitter-ziggy-schema/src/parser.c' },
+    --         branch = 'main',
+    --         generate_requires_npm = false,
+    --         requires_generate_from_grammar = false,
+    --       },
+    --       filetype = 'ziggy-schema',
+    --     }
+    --
+    --     parser_config.supermd = {
+    --       install_info = {
+    --         url = 'https://github.com/kristoff-it/supermd',
+    --         includes = { 'tree-sitter/supermd/src' },
+    --         files = {
+    --           'tree-sitter/supermd/src/parser.c',
+    --           'tree-sitter/supermd/src/scanner.c',
+    --         },
+    --         branch = 'main',
+    --         generate_requires_npm = false,
+    --         requires_generate_from_grammar = false,
+    --       },
+    --       filetype = 'supermd',
+    --     }
+    --
+    --     parser_config.supermd_inline = {
+    --       install_info = {
+    --         url = 'https://github.com/kristoff-it/supermd',
+    --         includes = { 'tree-sitter/supermd-inline/src' },
+    --         files = {
+    --           'tree-sitter/supermd-inline/src/parser.c',
+    --           'tree-sitter/supermd-inline/src/scanner.c',
+    --         },
+    --         branch = 'main',
+    --         generate_requires_npm = false,
+    --         requires_generate_from_grammar = false,
+    --       },
+    --       filetype = 'supermd_inline',
+    --     }
+    --
+    --     parser_config.superhtml = {
+    --       install_info = {
+    --         url = 'https://github.com/kristoff-it/superhtml',
+    --         includes = { 'tree-sitter-superhtml/src' },
+    --         files = {
+    --           'tree-sitter-superhtml/src/parser.c',
+    --           'tree-sitter-superhtml/src/scanner.c',
+    --         },
+    --         branch = 'main',
+    --         generate_requires_npm = false,
+    --         requires_generate_from_grammar = false,
+    --       },
+    --       filetype = 'superhtml',
+    --     }
+    --
+    --     vim.filetype.add {
+    --       extension = {
+    --         smd = 'supermd',
+    --         shtml = 'superhtml',
+    --         ziggy = 'ziggy',
+    --         ['ziggy-schema'] = 'ziggy_schema',
+    --         struct = 'st',
+    --         enum = 'st',
+    --       },
+    --     }
+    --   end,
   },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
--- lua/custom/plugins/iec61131-st.lua
+++ lua/custom/plugins/iec61131-st.lua
@@ -1,14 +1,20 @@
-local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
-parser_config.structured_text = {
-  install_info = {
-    url = 'c:/src/tree-sitter-structured-text',
-    -- url = "https://github.com/tanj/tree-sitter-structured-text.git",
-    files = { 'src/parser.c' },
-    branch = 'ts-upgrade',
-    generate_requires_npm = false,
-    requires_generate_from_grammar = false,
-  },
-  filetype = 'st',
-}
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'TSUpdate',
+  callback = function()
+    parser_config = require 'nvim-treesitter.parsers'
+    parser_config.structured_text = {
+      install_info = {
+        path = 'c:/src/tree-sitter-structured-text',
+        -- url = "https://github.com/tanj/tree-sitter-structured-text.git",
+        files = { 'src/parser.c' },
+        branch = 'ts-upgrade',
+        generate_requires_npm = false,
+        requires_generate_from_grammar = false,
+      },
+      filetype = 'st',
+    }
+    vim.treesitter.language.register('structured_text', { 'st' })
+  end,
+})
 local M = {}
 return M

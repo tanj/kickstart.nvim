d6ff75458238067f9f083c368a5ff9bd81df3667
John te Bokkel <tanj.tanj@gmail.com>
2026-01-21 05:00:12 -0800

    fix: default install path should be url not path

--- lua/custom/plugins/iec61131-st.lua
+++ lua/custom/plugins/iec61131-st.lua
@@ -4,8 +4,8 @@ vim.api.nvim_create_autocmd('User', {
     parser_config = require 'nvim-treesitter.parsers'
     parser_config.structured_text = {
       install_info = {
-        path = 'c:/src/tree-sitter-structured-text',
-        -- url = "https://github.com/tanj/tree-sitter-structured-text.git",
+        -- path = 'c:/src/tree-sitter-structured-text',
+        url = 'https://github.com/tanj/tree-sitter-structured-text.git',
         files = { 'src/parser.c' },
         branch = 'ts-upgrade',
         generate_requires_npm = false,

9c69365485226f8f7eb5c82b8ce12e354b886b9c
John te Bokkel <tanj.tanj@gmail.com>
2026-06-11 11:34:40 -0700

    feat: telescope list files in current file's dir

--- init.lua
+++ init.lua
@@ -520,7 +520,14 @@ require('lazy').setup({
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [f]iles' })
+      vim.keymap.set('n', '<leader>sF', function()
+        require('telescope.builtin').find_files {
+          cwd = vim.fn.expand '%:p:h',
+          -- hidden = false,
+          find_command = { 'fd', '--type', 'f', '--max-depth', '1', '--hidden', '--no-ignore' },
+        }
+      end, { desc = '[S]earch [F]iles in this files dir' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })

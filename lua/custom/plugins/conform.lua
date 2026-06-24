d9a3bd38f53db24a41a9e47b3f5c21d26aeca8c3
John te Bokkel <tanj.tanj@gmail.com>
2026-06-24 14:46:13 -0700

    fix: FormatDisable makes more sense to be buffer local by default

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -1,9 +1,10 @@
 vim.api.nvim_create_user_command('FormatDisable', function(args)
   if args.bang then
-    -- FormatDisable! will disable formatting just for this buffer
-    vim.b.disable_autoformat = true
-  else
+    -- FormatDisable! will disable formatting globally
     vim.g.disable_autoformat = true
+  else
+    -- FormatDisable will disable just for this buffer
+    vim.b.disable_autoformat = true
   end
 end, {
   desc = 'Disable autoformat-on-save',

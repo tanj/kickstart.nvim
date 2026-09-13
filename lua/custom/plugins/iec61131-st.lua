8a684f454ec9d75fd522bf7c0af479d08a2699db
John te Bokkel <tanj.tanj@gmail.com>
2026-09-12 17:51:48 -0700

    feat: update iec61131 config

--- lua/custom/plugins/iec61131-st.lua
+++ lua/custom/plugins/iec61131-st.lua
@@ -1,20 +1,53 @@
+-- 1. Register filetypes globally FIRST so filetypes exist when opening files
+vim.filetype.add {
+  extension = {
+    st = 'st',
+    struct = 'struct',
+    gvl = 'gvl',
+    enum = 'enum',
+    itf = 'itf',
+    meth = 'meth',
+  },
+}
+
+-- 2. Configure Treesitter parser on TSUpdate
 vim.api.nvim_create_autocmd('User', {
   pattern = 'TSUpdate',
   callback = function()
-    parser_config = require 'nvim-treesitter.parsers'
+    local parser_config = require 'nvim-treesitter.parsers'
     parser_config.structured_text = {
       install_info = {
-        -- path = 'c:/src/tree-sitter-structured-text',
         url = 'https://github.com/tanj/tree-sitter-structured-text.git',
         files = { 'src/parser.c' },
-        branch = 'ts-upgrade',
+        branch = 'main',
         generate_requires_npm = false,
         requires_generate_from_grammar = false,
       },
       filetype = 'st',
     }
-    vim.treesitter.language.register('structured_text', { 'st' })
+    vim.treesitter.language.register('structured_text', { 'st', 'struct', 'gvl', 'enum', 'itf', 'meth' })
   end,
 })
-local M = {}
+
+-- This didn't seem to do much, maybe look into this in the future
+-- go install github.com/joyautomation/nautilus/cmd/nautilus@latest
+local M = {
+  -- 'iec61131-lsp',
+  -- dir = vim.fn.stdpath 'config', -- Tells Lazy this is a local plugin
+  -- ft = { 'st', 'struct', 'gvl', 'enum', 'itf', 'meth' },
+  -- config = function()
+  --   vim.api.nvim_create_autocmd('FileType', {
+  --     -- List all filetypes you registered above
+  --     pattern = { 'st', 'struct', 'gvl', 'enum', 'itf', 'meth' },
+  --     callback = function(ev)
+  --       vim.lsp.start {
+  --         name = 'nautilus',
+  --         cmd = { 'nautilus', 'lsp' },
+  --         root_dir = vim.fs.root(ev.buf, { 'nautilus.yaml', '.git', '*.fbslib', '*.fbsproj' }),
+  --       }
+  --     end,
+  --   })
+  -- end,
+}
+
 return M
